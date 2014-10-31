9cd8b77b83f8ad5adcd41a32ad2be7c27923c997
mirianpereira <mirianpereira.mp@gmail.com>
2014-10-31 14:19:08 -0200

    Update programa_calcular.py

--- programa_calcular.py
+++ programa_calcular.py
@@ -1,18 +1,34 @@
-def soma():
-      pass
-def subtração():
-      pass
-def divisão():
-      pass
-def multiplicação():
-      pass
+ ("Entre com o valor # 2")
+from tkinter import *
 
-def programa_principal():
-      print("Bem Vindo a calculadora")
-      x = input ("Entre com o valor  # 1")
-      y = input ("Entre com o valor # 2")
-      operação = input ("Qual operação?")
+def principal():
+	# definição da janela
+	janela = Tk()
 
-      if operação == '+':
-            soma()
+	# definindo o título da janela
+	janela.title("Exemplo #1")
+	
+	# componentes
+	botão1 = Button(janela, bg="red", fg="white", text = "Botão #1")
+	botão2 = Button(janela, bg="blue", fg="yellow", text = "Botão #2" )
+	
+
+	# organização na tela
+	botão1.pack()
+	botão2.pack()
+	
+	# definindo "ações" para os botões
+	botão1.configure(command=click_botão1)
+	botão2.configure(command=click_botão2)
+
+# ações
+def click_botão1():
+	print("evento: click no botão 1")
+
+def click_botão2():
+	print("evento: click no botão 2")
+
+# rodando nosso programa
+if __name__ == '__main__':
+	principal()
       
