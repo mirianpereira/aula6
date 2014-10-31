beda6f9d43d970ed8f7440125797249229fe2e7c
mirianpereira <mirianpereira.mp@gmail.com>
2014-10-31 08:42:15 -0200

    aula
    empresas

---
+++ programa_calcular.py
@@ -0,0 +1,18 @@
+def soma():
+      pass
+def subtração():
+      pass
+def divisão():
+      pass
+def multiplicação():
+      pass
+
+def programa_principal():
+      print("Bem Vindo a calculadora")
+      x = input ("Entre com o valor  # 1")
+      y = input ("Entre com o valor # 2")
+      operação = input ("Qual operação?")
+
+      if operação == '+':
+            soma()
+      
